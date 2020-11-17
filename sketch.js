20be4e9d48143ba433dcb55c15abc0fb94418303
kavyamittal1234 <kavyamittal050@gmail.com>
2020-11-17 13:41:48 +0530

    kavya

--- sketch.js
+++ sketch.js
@@ -1,29 +1,27 @@
-var ball;
-
+var canvas ,backgroungImage;
+var gameState=0;
+var playerCount;
+var allPlayers;
+var distance=0;
+var database;
+var form,player,game;
 function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    canvas=createCanvas(400,400);
+    database=firebase.database();
+game=new Game();
+game.getState();
+game.start();
+
+
+
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
+if(playerCount===4){
+    game.update(1);
 }
-
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+if(gameState===1){
+    clear();
+    game.play();
+}
 }
